e1b99e951765aa1e099570ff66ed683fbdf2c5ca
AleksandrSpiridonov <spiridon-2005@mail.ru>
2026-03-25 22:26:50 +0300

    languages scripts extended

--- judge0_plug/lang/en/qtype_judge0.php
+++ judge0_plug/lang/en/qtype_judge0.php
@@ -7,3 +7,22 @@ $string['pluginnameediting'] = 'Editing a Judge0 Question';
 $string['pluginnamesummary'] = 'Evaluates student code via Judge0.';
 $string['checker_code'] = 'Checker Code';
 $string['checker_code_help'] = 'This code is appended to the student\'s code before execution. Use it to call student functions and print results.';
+$string['expected_output'] = 'Expected Output';
+$string['language_id'] = 'Programming Language';
+$string['language_id_help'] = 'Select the programming language students will use.';
+$string['server_url'] = 'Judge0 Server URL';
+$string['server_url_desc'] = 'Judge0 API address, e.g.: http://server:2358';
+
+$string['reference_solution'] = 'Reference Solution';
+$string['reference_solution_help'] = 'Reference code that generates the correct output for test cases.';
+$string['testcases'] = 'Test Cases';
+$string['test_input'] = 'Standard Input (stdin)';
+$string['test_expected_output'] = 'Expected Output (stdout)';
+$string['is_hidden'] = 'Hidden Test';
+$string['testcase_weight'] = 'Test Weight';
+$string['add_testcase'] = 'Add another test case';
+
+$string['input_generator_header'] = 'Dynamic Input Generation (Optional)';
+$string['input_generator_code'] = 'Input Generator Code';
+$string['input_generator_code_help'] = 'A script that receives the student ID via standard input (stdin) and outputs unique test data. Leave empty if dynamic generation is not needed.';
+$string['input_generator_language_id'] = 'Generator Programming Language';
