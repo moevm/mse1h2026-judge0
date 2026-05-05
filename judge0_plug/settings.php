96c90b599bfb3282e6421fe270d6fd89af603e47
Alexandr Spiridonov <144447412+ReMAC20@users.noreply.github.com>
2026-05-05 23:07:36 +0300

    admin settings for monaco and judge0
    Signed-off-by: Alexandr Spiridonov <144447412+ReMAC20@users.noreply.github.com>

--- judge0_plug/settings.php
+++ judge0_plug/settings.php
@@ -9,4 +9,35 @@ if ($ADMIN->fulltree) {
         'http://server:2358',
         PARAM_URL
     ));
+
+    $settings->add(new admin_setting_configtext(
+        'qtype_judge0/auth_header',
+        get_string('auth_header', 'qtype_judge0'),
+        get_string('auth_header_desc', 'qtype_judge0'),
+        '',
+        PARAM_ALPHANUMEXT
+    ));
+
+    $settings->add(new admin_setting_configpasswordunmask(
+        'qtype_judge0/auth_token',
+        get_string('auth_token', 'qtype_judge0'),
+        get_string('auth_token_desc', 'qtype_judge0'),
+        ''
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'qtype_judge0/poll_timeout',
+        get_string('poll_timeout', 'qtype_judge0'),
+        get_string('poll_timeout_desc', 'qtype_judge0'),
+        45,
+        PARAM_INT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'qtype_judge0/monaco_base_url',
+        get_string('monaco_base_url', 'qtype_judge0'),
+        get_string('monaco_base_url_desc', 'qtype_judge0'),
+        '',
+        PARAM_URL
+    ));
 }
