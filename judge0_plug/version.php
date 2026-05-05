267915b9d86cd1c046d5567e54884c88914e559f
Alexandr Spiridonov <144447412+ReMAC20@users.noreply.github.com>
2026-05-05 23:02:18 +0300

    version slightly updated
    Signed-off-by: Alexandr Spiridonov <144447412+ReMAC20@users.noreply.github.com>

--- judge0_plug/version.php
+++ judge0_plug/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_judge0';
-$plugin->version   = 2026032238; 
+$plugin->version   = 2026050503; 
 $plugin->requires  = 2022041900;
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.4';
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = '0.6.2';
